3e16fdff55b69dfac1796d3898699a8d1f4cdaa7
grimmjow22 <mamarvinadan@gmail.com>
2024-02-09 07:10:16 -0500

    Marvin

--- src/App.js
+++ src/App.js
@@ -1,51 +1,48 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute.js';
-import Login from './components/Login/Login.js';
-import Header from './components/Header/Header.js';
-import Buttons from './components/Buttons/Buttons.js';
-import Table from './components/Table/Table.js';
-import ServicesTable from './components/ServicesTable/ServicesTable.js';
-import PartsTable from './components/PartsTable/PartsTable.js';
-import TableMecanical from './components/TableMecanical/TableMecanical.jsx';
+import { useAuth0 } from "@auth0/auth0-react";
+import PrivateRoute from './PrivateRoute';
+import LoginButton from './components/Login/Login'; // Asegúrate de que esta ruta de importación sea correcta
+import Header from './components/Header/Header';
+import Buttons from './components/Buttons/Buttons';
+import Table from './components/Table/Table';
+import ServicesTable from './components/ServicesTable/ServicesTable';
+import PartsTable from './components/PartsTable/PartsTable';
+import TableMecanical from './components/TableMecanical/TableMecanical';
+
+// Componentes adicionales
+import LogoutButton from "./components/Logout/Logout";
+import Profile from "./components/Profile/Profile";
+import AfterLoginRedirect from './components/Roles/Roles'; // Ajusta según la ubicación real
 
 function App() {
+  const { isLoading, isAuthenticated } = useAuth0();
   const [authenticated, setAuthenticated] = useState(false);
 
-  const handleLogin = () => {
-    console.log('Usuario ha iniciado sesión');
-    setAuthenticated(true);
+  if (isLoading) return <div>Cargando...</div>;
+
+  const handleAuthentication = (isAuthenticated) => {
+    console.log(isAuthenticated ? 'Usuario ha iniciado sesión' : 'Usuario no autenticado');
+    setAuthenticated(isAuthenticated);
   };
 
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Login onLogin={handleLogin} />} />
-          <Route
-            path="/Header"
-            element={<PrivateRoute element={<Header />} isAuthenticated={authenticated} />}
-          />
-          <Route
-            path="/Buttons"
-            element={<PrivateRoute element={<Buttons />} isAuthenticated={authenticated} />}
-          />
-          <Route
-            path="/Mecanicos"
-            element={<PrivateRoute element={<Table />} isAuthenticated={authenticated} />}
-          />
-          <Route
-            path="/Servicios"
-            element={<PrivateRoute element={<ServicesTable />} isAuthenticated={authenticated} />}
-          />
-          <Route
-            path="/Piezas"
-            element={<PrivateRoute element={<PartsTable />} isAuthenticated={authenticated} />}
-          />
-          <Route
-            path="/VistaMec"
-            element={<PrivateRoute element={<TableMecanical />} isAuthenticated={authenticated} />}
-          />
+          <Route path="/" element={
+            isAuthenticated ? 
+            <AfterLoginRedirect onAuthenticated={handleAuthentication} /> : 
+            <LoginButton onLogin={handleAuthentication} />
+          }/>
+          <Route path="/perfil" element={<PrivateRoute element={<Profile />} isAuthenticated={isAuthenticated} />} />
+          <Route path="/cerrar-sesion" element={<PrivateRoute element={<LogoutButton />} isAuthenticated={isAuthenticated} />} />
+          <Route path="/Header" element={<PrivateRoute element={<Header />} isAuthenticated={isAuthenticated} />} />
+          <Route path="/Buttons" element={<PrivateRoute element={<Buttons />} isAuthenticated={isAuthenticated} />} />
+          <Route path="/Mecanicos" element={<PrivateRoute element={<Table />} isAuthenticated={isAuthenticated} />} />
+          <Route path="/Servicios" element={<PrivateRoute element={<ServicesTable />} isAuthenticated={isAuthenticated} />} />
+          <Route path="/Piezas" element={<PrivateRoute element={<PartsTable />} isAuthenticated={isAuthenticated} />} />
+          <Route path="/VistaMec" element={<PrivateRoute element={<TableMecanical />} isAuthenticated={isAuthenticated} />} />
         </Routes>
       </Router>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.js';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
+import "./index.css";
 
-const root = document.getElementById('root');
+const root = createRoot(document.getElementById("root"));
 
-// Utiliza createRoot desde react-dom/client
-const rootInstance = createRoot(root);
-rootInstance.render(
+root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="marvinadan.us.auth0.com"
+      clientId="Nof33GJEERON3PlF2tE20HSKlBKZxoAO"
+      redirect_uri="https://231e-201-134-180-249.ngrok-free.app"
+      audience="https://Marvin-Api-Endpoint/"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
-
-
-
